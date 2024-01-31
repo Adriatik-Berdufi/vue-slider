4949528fb95aa207ec6d05c0ad01b6829532f651
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-01-31 09:38:41 +0100

    feat: ottimazione del codice

--- main.js
+++ main.js
@@ -1,52 +1,18 @@
 const {createApp} = Vue;
 const app = createApp({
-
     data(){
         return{
             activeImage: 0,
-            images : [
-              './img/01.webp',
-              './img/02.webp', 
-              './img/03.webp', 
-              './img/04.webp', 
-              './img/05.webp' ,
-            ]
-          
+            images : ['./img/01.webp','./img/02.webp', './img/03.webp', './img/04.webp', './img/05.webp' ,]
         };
     },
-
     methods: {
-        nextImage(){
-            if(this.activeImage >= this.images.length - 1){
-                this.activeImage = 0;
-            }else{
-                this.activeImage++;
-            };
-        },
-
-        prevImage(){
-            if(this.activeImage <= 0){
-                this.activeImage = this.images.length - 1;
-
-            }else{
-                this.activeImage--;
-            };
-        },
-
-        thumb(index){
-            this.activeImage = index;
-        },
-
-        outoPlay(){
-            this.autoplay = setInterval(() => {
-            this.nextImage();
-        }, 1000);
-        },    
-        stopAutoPlay(){
-            clearInterval(this.autoplay);
-        },
+        nextImage(){this.activeImage = (this.activeImage >= this.images.length - 1) ? 0 : this.activeImage + 1;},
+        prevImage(){this.activeImage = (this.activeImage <= 0) ? this.images.length - 1 : 0;},
+        thumb(index){this.activeImage = index;},
+        outoPlay(){this.autoplay = setInterval(() => {this.nextImage();}, 1000);},    
+        stopAutoPlay(){clearInterval(this.autoplay);},
     },
-
 });
 app.mount('#app');
 
